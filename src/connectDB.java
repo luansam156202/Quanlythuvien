f0be9db8acac46a36a29c7bd05a534b563585398
Kwaiii2 <Hoanchu54@gmail.com>
2024-11-21 12:57:38 +0700

    connect db

---
+++ src/connectDB.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class connectDB {
+    public static void main(String[] args) {
+        // Thông tin kết nối
+        String url = "jdbc:mysql://localhost:3306/librarydb"; // Thay your_database_name bằng tên DB
+        String username = "root"; // Thay bằng username của bạn
+        String password = "Quang7864@@"; // Thay bằng password của bạn
+
+        try {
+
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Kết nối thành công!");
+
+
+
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Kết nối thất bại!");
+        }
+    }
+}
